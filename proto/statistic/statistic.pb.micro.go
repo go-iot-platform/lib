05e80fddabcf424e69440e5b22c20500544d9608
thoverik <tho.pham@veriksystems.com>
2020-11-19 16:28:04 +0700

    update statistic

--- proto/statistic/statistic.pb.micro.go
+++ proto/statistic/statistic.pb.micro.go
@@ -43,19 +43,8 @@ func NewSystemSvcEndpoints() []*api.Endpoint {
 // Client API for SystemSvc service
 
 type SystemSvcService interface {
-	GetCallingActivating(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	GetCountAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	GetMapDirectionAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	SendObserverEmailAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
 	GetChartData(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
 	HistoryBySerial(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	HeartRateStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	SnoringStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	SleepStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	SleepMonthStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	SleepWeekStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	BedSensorLog(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
-	TimeLine(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error)
 }
 
 type systemSvcService struct {
@@ -68,46 +57,6 @@ func NewSystemSvcService(name string, c client.Client) SystemSvcService {
 		c:    c,
 		name: name,
 	}
-}
-
-func (c *systemSvcService) GetCallingActivating(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.GetCallingActivating", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) GetCountAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.GetCountAPI", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) GetMapDirectionAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.GetMapDirectionAPI", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) SendObserverEmailAPI(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.SendObserverEmailAPI", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
 }
 
 func (c *systemSvcService) GetChartData(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
@@ -130,109 +79,17 @@ func (c *systemSvcService) HistoryBySerial(ctx context.Context, in *api1.Request
 	return out, nil
 }
 
-func (c *systemSvcService) HeartRateStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.HeartRateStatistic", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) SnoringStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.SnoringStatistic", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) SleepStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.SleepStatistic", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) SleepMonthStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.SleepMonthStatistic", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) SleepWeekStatistic(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.SleepWeekStatistic", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) BedSensorLog(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.BedSensorLog", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *systemSvcService) TimeLine(ctx context.Context, in *api1.Request, opts ...client.CallOption) (*api1.Response, error) {
-	req := c.c.NewRequest(c.name, "SystemSvc.TimeLine", in)
-	out := new(api1.Response)
-	err := c.c.Call(ctx, req, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // Server API for SystemSvc service
 
 type SystemSvcHandler interface {
-	GetCallingActivating(context.Context, *api1.Request, *api1.Response) error
-	GetCountAPI(context.Context, *api1.Request, *api1.Response) error
-	GetMapDirectionAPI(context.Context, *api1.Request, *api1.Response) error
-	SendObserverEmailAPI(context.Context, *api1.Request, *api1.Response) error
 	GetChartData(context.Context, *api1.Request, *api1.Response) error
 	HistoryBySerial(context.Context, *api1.Request, *api1.Response) error
-	HeartRateStatistic(context.Context, *api1.Request, *api1.Response) error
-	SnoringStatistic(context.Context, *api1.Request, *api1.Response) error
-	SleepStatistic(context.Context, *api1.Request, *api1.Response) error
-	SleepMonthStatistic(context.Context, *api1.Request, *api1.Response) error
-	SleepWeekStatistic(context.Context, *api1.Request, *api1.Response) error
-	BedSensorLog(context.Context, *api1.Request, *api1.Response) error
-	TimeLine(context.Context, *api1.Request, *api1.Response) error
 }
 
 func RegisterSystemSvcHandler(s server.Server, hdlr SystemSvcHandler, opts ...server.HandlerOption) error {
 	type systemSvc interface {
-		GetCallingActivating(ctx context.Context, in *api1.Request, out *api1.Response) error
-		GetCountAPI(ctx context.Context, in *api1.Request, out *api1.Response) error
-		GetMapDirectionAPI(ctx context.Context, in *api1.Request, out *api1.Response) error
-		SendObserverEmailAPI(ctx context.Context, in *api1.Request, out *api1.Response) error
 		GetChartData(ctx context.Context, in *api1.Request, out *api1.Response) error
 		HistoryBySerial(ctx context.Context, in *api1.Request, out *api1.Response) error
-		HeartRateStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error
-		SnoringStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error
-		SleepStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error
-		SleepMonthStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error
-		SleepWeekStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error
-		BedSensorLog(ctx context.Context, in *api1.Request, out *api1.Response) error
-		TimeLine(ctx context.Context, in *api1.Request, out *api1.Response) error
 	}
 	type SystemSvc struct {
 		systemSvc
@@ -245,54 +102,10 @@ type systemSvcHandler struct {
 	SystemSvcHandler
 }
 
-func (h *systemSvcHandler) GetCallingActivating(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.GetCallingActivating(ctx, in, out)
-}
-
-func (h *systemSvcHandler) GetCountAPI(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.GetCountAPI(ctx, in, out)
-}
-
-func (h *systemSvcHandler) GetMapDirectionAPI(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.GetMapDirectionAPI(ctx, in, out)
-}
-
-func (h *systemSvcHandler) SendObserverEmailAPI(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.SendObserverEmailAPI(ctx, in, out)
-}
-
 func (h *systemSvcHandler) GetChartData(ctx context.Context, in *api1.Request, out *api1.Response) error {
 	return h.SystemSvcHandler.GetChartData(ctx, in, out)
 }
 
 func (h *systemSvcHandler) HistoryBySerial(ctx context.Context, in *api1.Request, out *api1.Response) error {
 	return h.SystemSvcHandler.HistoryBySerial(ctx, in, out)
-}
-
-func (h *systemSvcHandler) HeartRateStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.HeartRateStatistic(ctx, in, out)
-}
-
-func (h *systemSvcHandler) SnoringStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.SnoringStatistic(ctx, in, out)
-}
-
-func (h *systemSvcHandler) SleepStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.SleepStatistic(ctx, in, out)
-}
-
-func (h *systemSvcHandler) SleepMonthStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.SleepMonthStatistic(ctx, in, out)
-}
-
-func (h *systemSvcHandler) SleepWeekStatistic(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.SleepWeekStatistic(ctx, in, out)
-}
-
-func (h *systemSvcHandler) BedSensorLog(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.BedSensorLog(ctx, in, out)
-}
-
-func (h *systemSvcHandler) TimeLine(ctx context.Context, in *api1.Request, out *api1.Response) error {
-	return h.SystemSvcHandler.TimeLine(ctx, in, out)
 }
